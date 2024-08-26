e1cb056036c8c928539a8ff80bd645899d05af25
HP <HP@DESKTOP-66A8FE7.mshome.net>
2024-08-26 09:40:51 +0530

    meghaaaa

---
+++ Actitime2/src/test/java/com/Actitime2/testscripts/classyy.java
@@ -0,0 +1,21 @@
+package com.Actitime2.testscripts;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class classyy {
+			
+			@Test
+			public void createCustomerTest() throws InterruptedException 
+			{
+				WebDriver driver=new ChromeDriver();
+				driver.manage().window().maximize();
+				driver.get("https://youtube.com/");
+				Reporter.log("customer created successfully",true);
+				Thread.sleep(3000);
+				driver.close();
+			}
+
+		}
